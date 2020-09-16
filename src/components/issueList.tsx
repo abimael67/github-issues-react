caca550e28833812f333cf650b044c90533bcd8b
Jose A Martinez <jamroa67@gmail.com>
2020-09-16 19:38:47 -0400

    fixed bug when no matching text and press enter

--- src/components/issueList.tsx
+++ src/components/issueList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Issue } from "../types/Issue";
 import "../assets/autocomplete.css";
 import { IssueSuggestion } from "./issueSuggestion";
@@ -7,67 +7,47 @@ export const IssueList: React.FC<{
   issueList: Array<Issue>;
   selectIssue: (issue: Issue) => void;
 }> = ({ issueList, selectIssue }) => {
-  const selectedIssueIndexRef = useRef<number>(0);
   const [selectedIssueIndex, setSelectedIssueIndex] = useState<number>(0);
-  /**Handle the mouse movement event to select the hovered issue */
+
   const setSelectedIssueIndexWithMouse = (
     e: React.MouseEvent<HTMLLIElement, MouseEvent>
   ) => {
     e.preventDefault();
     setSelectedIssueIndex(Number(e.currentTarget.dataset.index));
   };
-  /**Retrieves the current selected issue and send it back to parent's
-   *  selectIssue function. */
+
   const retrieveIssue = () => {
-    selectIssue(issueList[selectedIssueIndex]);
+    if (issueList.length > 0) selectIssue(issueList[selectedIssueIndex]);
   };
-  /**Manages the key press events subscribing the listener callbacks
-   * when the component is first mounted and unsubscribe when it gets
-   * unmounted.
-   */
-  useEffect(() => {
-    window.addEventListener("keydown", (event: KeyboardEvent) => {
-      if (event.defaultPrevented) {
-        return;
-      }
-      switch (event.key) {
-        case "ArrowUp":
-          selectedIssueIndexRef.current =
-            selectedIssueIndexRef.current > 0
-              ? selectedIssueIndexRef.current - 1
-              : 0;
-          setSelectedIssueIndex(selectedIssueIndexRef.current);
-          break;
-        case "ArrowDown":
-          selectedIssueIndexRef.current = selectedIssueIndexRef.current + 1;
-          setSelectedIssueIndex(selectedIssueIndexRef.current);
-          break;
-      }
-    });
 
-    return () => {
-      window.removeEventListener("keydown", () => null);
-    };
-  }, []);
-  /**Manages the key 'Enter' subscribing the listener callback
-   * when the component is updated and unsubscribe when it gets
-   * unmounted.
-   */
   useEffect(() => {
-    window.addEventListener("keydown", (event: KeyboardEvent) => {
-      if (event.defaultPrevented) return;
-
-      switch (event.key) {
-        case "Enter":
-          retrieveIssue();
-          break;
-      }
-    });
+    window.addEventListener("keydown", arrowKeysHandler);
     return () => {
-      window.removeEventListener("keydown", () => null);
+      window.removeEventListener("keydown", arrowKeysHandler);
     };
   });
-
+  const arrowKeysHandler = (event: KeyboardEvent) => {
+    if (event.defaultPrevented) {
+      return;
+    }
+    switch (event.key) {
+      case "ArrowUp":
+        setSelectedIssueIndex(
+          selectedIssueIndex > 0 ? selectedIssueIndex - 1 : 0
+        );
+        break;
+      case "ArrowDown":
+        setSelectedIssueIndex(
+          selectedIssueIndex < issueList.length - 1
+            ? selectedIssueIndex + 1
+            : selectedIssueIndex
+        );
+        break;
+      case "Enter":
+        retrieveIssue();
+        break;
+    }
+  };
   return (
     <ul className="issues">
       {issueList &&
