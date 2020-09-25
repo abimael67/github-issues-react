22dd665b95ab500e8b3e14b644c4966c261e80a5
Jose A Martinez <jamroa67@gmail.com>
2020-09-25 05:17:10 -0400

    fixed quote linting problem

--- src/api/github.ts
+++ src/api/github.ts
@@ -2,7 +2,7 @@ import { Issue } from "../types/Issue";
 /**Basic function that calls github issues API endpoint and returns an array of Issues */
 export const getIssues = async (): Promise<Array<Issue>> => {
   let results = await fetch(
-    `https://api.github.com/repos/facebook/react/issues`
+    "https://api.github.com/repos/facebook/react/issues"
   ).then((resp) => resp.json());
   return results as Array<Issue>;
 };
